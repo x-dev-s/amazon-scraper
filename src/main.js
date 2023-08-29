e4abf94445b6c9c1d1598ff9df910abb59d048ac
Abdullah Sultan <abdullahsultan546@gmail.com>
2023-08-29 09:55:30 +0500

    updates

--- src/main.js
+++ src/main.js
@@ -1,28 +1,52 @@
 // main.js
-import { CheerioCrawler, KeyValueStore, log, Dataset} from 'crawlee';
+import UserAgent from 'user-agents';
+import { CheerioCrawler, ProxyConfiguration, KeyValueStore, log, Dataset } from 'crawlee';
 import { router } from './routes.js';
 import { BASE_URL } from './constants.js';
 
-// Grab our keyword from the input
+const proxyConfiguration = new ProxyConfiguration({
+    proxyUrls: [
+        'http://proxy-1.com',
+        'http://proxy-2.com',
+    ],
+});
+const userAgent = new UserAgent({
+    deviceCategory: 'desktop'
+});
+console.log(userAgent.toString());
 const { keyword } = await KeyValueStore.getInput();
 const { page } = await KeyValueStore.getInput();
 const { sort } = await KeyValueStore.getInput();
 const crawler = new CheerioCrawler({
+    // proxyConfiguration,
+    // useSessionPool: true,
+    // persistCookiesPerSession: true,
+    // async requestHandler({ session, $ }) {
+    //     const title = $('title').text();
+    //     if (title === 'Blocked') {
+    //         session.retire();
+    //     } else if (title === 'Not sure if blocked, might also be a connection error') {
+    //         session.markBad();
+    //     }
+    // },
+    minConcurrency: 5,
+    maxConcurrency: 15,
     requestHandler: router,
-});
-
-// Add our initial requests
-for (var i = 1; i <= page; i++){
-await crawler.addRequests([
-    {
-        // Turn the inputted keyword into a link we can make a request with
-        url: `${BASE_URL}/s?k=${keyword}&s=${sort}&page=${i}`,
-        label: 'START',
-        userData: {
-            keyword,
+    preNavigationHooks: [
+        function customUserAgent(_ctx, opts = {}) {
+            opts.headers = opts.headers || userAgent;
+            opts.useHeaderGenerator = false;
         },
-    },
-]);
+    ],
+});
+// Add our initial requests
+for (var i = 1; i <= page; i++) {
+    await crawler.addRequests([
+        {
+            url: `${BASE_URL}/s?k=${keyword}&s=${sort}&page=${i}`,
+            label: 'START',
+        },
+    ]);
 }
 log.info('Starting the crawl.');
 await crawler.run();
--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,12 @@ import { BASE_URL, labels } from './constants.js';
 
 export const router = createCheerioRouter();
 
-router.addHandler(labels.START, async ({ $, crawler, request }) => {
+router.addHandler(labels.START, async ({ $, crawler }) => {
     const products = $('div > div[data-asin]:not([data-asin=""])');
 
     for (const product of products) {
         const element = $(product);
         const titleElement = $(element.find('.a-text-normal[href]'));
-
         const url = `${BASE_URL}${titleElement.attr('href')}`;
 
         await crawler.addRequests([
@@ -30,7 +29,6 @@ router.addHandler(labels.START, async ({ $, crawler, request }) => {
 
 router.addHandler(labels.PRODUCT, async ({ $, crawler, request }) => {
     const { data } = request.userData;
-
     await crawler.addRequests([
         {
             url: `${BASE_URL}/gp/aod/ajax/ref=auto_load_aod?asin=${data.asin}&pc=dp`,
@@ -39,14 +37,13 @@ router.addHandler(labels.PRODUCT, async ({ $, crawler, request }) => {
                 data: {
                     ...data,
                     image: $("#imgTagWrapperId>img").attr('src'),
-                    availability: $('#availability>span').text().trim(),
-                    price: '$' + $('.a-price .a-price-whole').first().text().trim() + $('.a-price .a-price-fraction').first().text().trim(),
-                    saving: $('span.savingPriceOverride').text().trim(),
+                    availability: $('#availability>span').text().trim() || $('#outOfStock .a-text-bold').text().trim() || $('#deliveryBlockMessage .a-color-error').text().trim(),
+                    price: ($('.a-price .a-price-whole').first().text().trim() + $('.a-price .a-price-fraction').first().text().trim()) || $('#price_inside_buybox').text().trim(),
+                    saving: $('span.savingPriceOverride').first().text().trim(),
                     rating: $('#acrPopover>span>a>span').first().text().trim(),
-                    ratingCount: $('#acrCustomerReviewText').first().text().trim(),
-                    bestSeller: $('div.zg-badge-wrapper i').text().trim(),
-                    amazonChoice: $('span.ac-badge-rectangle').text().trim(),
-                    description: $('#productDescription').text().trim(),
+                    reviews: $('#acrCustomerReviewText').first().text().trim(),
+                    tag: $('div.zg-badge-wrapper i').text().trim() || $('span.ac-badge-rectangle').text().trim(),
+                    description: $('#productDescription').text().trim() || $('#feature-bullets').text().trim() || $('.bundle-comp-bullets').text().trim(),
                 },
             },
         },
@@ -55,13 +52,7 @@ router.addHandler(labels.PRODUCT, async ({ $, crawler, request }) => {
 
 router.addHandler(labels.OFFERS, async ({ $, request }) => {
     const { data } = request.userData;
-
-    for (const offer of $('#aod-offer')) {
-        const element = $(offer);
-
-        await Dataset.pushData({
-            ...data,
-            sellerName: element.find('div[id*="soldBy"] a[aria-label]').text().trim(),
-        });
-    }
+    await Dataset.pushData({
+        ...data,
+    });
 });
